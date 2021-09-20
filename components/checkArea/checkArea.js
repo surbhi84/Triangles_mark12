7328e2bfd81caf9e53241b7e0d553f130ebe1611
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-21 00:02:18 +0530

    feat: area calculated

--- components/checkArea/checkArea.js
+++ components/checkArea/checkArea.js
@@ -2,3 +2,43 @@ var base = document.querySelector("#triangle-base");
 var height = document.querySelector("#triangle-height");
 var checkBtn = document.querySelector("#check-btn");
 var outputDiv = document.querySelector("#output-div");
+
+checkBtn.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  console.log(base.value, height.value);
+  const input = inputValue();
+
+  if (inputValidation(input)) {
+    const result = calculateArea(input);
+    output(result);
+  } else {
+    errorAlert();
+  }
+}
+
+function inputValue() {
+  return {
+    b: base.value,
+    h: height.value,
+  };
+}
+
+function inputValidation(input) {
+  if (input.b == "" || input.h == "") return false;
+  return true;
+}
+
+function calculateArea(input) {
+  let area = (parseFloat(input.b) * parseFloat(input.h)) / 2;
+  console.log(area);
+  return area;
+}
+
+function output(result) {
+  outputDiv.innerText = `The area of the triangle is ${result}`;
+}
+
+function errorAlert() {
+  outputDiv.innerText = "Please enter all the angles";
+}
