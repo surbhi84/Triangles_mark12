cf43a214aaa797aee0462a49602431dd183ba3c4
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-20 23:58:47 +0530

    feat: dom selection

--- components/checkArea/checkArea.js
+++ components/checkArea/checkArea.js
@@ -1,1 +1,4 @@
-
+var base = document.querySelector("#triangle-base");
+var height = document.querySelector("#triangle-height");
+var checkBtn = document.querySelector("#check-btn");
+var outputDiv = document.querySelector("#output-div");
---
+++ components/checkHypotenuse/checkHypotenuse.js
@@ -0,0 +1,4 @@
+var side1 = document.querySelector("#side-1");
+var side2 = document.querySelector("#side-2");
+var checkBtn = document.querySelector("#check-btn");
+var outputDiv = document.querySelector("#output-div");
